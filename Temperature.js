cf81cdfb468d4053104e5f15b3f1255ae5645f47
Pranav Arora <129372038+PranavArora20@users.noreply.github.com>
2025-01-09 17:27:53 +0000

    temperature question

---
+++ Temperature.js
@@ -0,0 +1,11 @@
+//Array Manipulation: Tracking Daily Temperatures
+//Create an array that stores the temperatures recorded over 7 days. 
+// Use indexing to access the temperature for the 4th day and update it to a new value by adding 2 degrees. 
+// After that, log the updated temperatures for all 7 days to the console.
+
+let arr = [12,21,32,21,10,8,19]
+arr[3]+=2
+// console.log(arr)
+for(let i =0;i<arr.length;i++){
+    console.log(arr[i])
+}
