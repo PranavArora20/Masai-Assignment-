686b66fc5e43c46d9cb586f3561bb91b4f87fcc5
Pranav Arora <129372038+PranavArora20@users.noreply.github.com>
2025-01-11 10:45:58 +0000

    to_do_list.js

---
+++ Arrays_And_Strings/to_do_list.js
@@ -0,0 +1,7 @@
+// Title: L0 - Managing a To-Do List: Adding and Removing Tasks
+// Problem Statement: Create an array that represents your daily to-do list with 5 tasks. Add a new task to the beginning of the list. 
+// Then, remove the last task from the list. Finally, log the updated to-do list to the console.
+
+let arr = ["task1","task2","task3","task4","task5"]
+arr.pop()
+console.log(arr)
