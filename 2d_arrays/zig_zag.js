96713451fda161533ef2f103a88c417a0efd45ee
Pranav Arora <129372038+PranavArora20@users.noreply.github.com>
2025-01-14 18:29:54 +0000

    zig_zag

---
+++ 2d_arrays/zig_zag.js
@@ -0,0 +1,25 @@
+// Given amatrixwith N rows and M columns. Print the matrix elements starting from the top right corner and follow a zig-zag pattern.
+
+// Look at the image for better understanding
+
+// [
+//     [1,2,3,4,5],
+//     [6,7,8,9,1],
+//     [3,2,4,5,6],
+//     [7,8,9,1,2]
+// ]
+
+let arr = [
+    [1,2,3,4,5],
+    [6,7,8,9,1],
+    [3,2,4,5,6],
+    [7,8,9,1,2]
+]
+
+for(let i =0;i<arr.length;i++){
+    if(i%2==0){
+        for(let j =arr.length-1;j>=0;j--){
+
+        }
+    }
+}
