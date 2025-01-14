d34bb77d4b448d47d0d2bb45c5f60d23d0fa41fd
Pranav Arora <129372038+PranavArora20@users.noreply.github.com>
2025-01-14 18:43:25 +0000

    zig zag updated

--- 2d_arrays/zig_zag.js
+++ 2d_arrays/zig_zag.js
@@ -12,14 +12,19 @@
 let arr = [
     [1,2,3,4,5],
     [6,7,8,9,1],
-    [3,2,4,5,6],
+    [3,2,5,4,6],
     [7,8,9,1,2]
 ]
-
+let output=""
 for(let i =0;i<arr.length;i++){
     if(i%2==0){
-        for(let j =arr.length-1;j>=0;j--){
-
+        for(let j =arr[i].length-1;j>=0;j--){
+            output+=arr[i][j] + " "
+        }
+    }else{
+        for(let j =0;j<arr[i].length;j++){
+            output+=arr[i][j] + " "
         }
     }
 }
+console.log(output)
